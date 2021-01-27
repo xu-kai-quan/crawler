086c8241033205cfd1804d62b01fb025c4694c50
XuKaiQuan <964262001@qq.com>
2021-01-27 21:34:13 +0800

    算法的初步实现

--- src/main/java/com/github/xuKaiQuan/Main.java
+++ src/main/java/com/github/xuKaiQuan/Main.java
@@ -6,21 +6,76 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://sina.cn/");
+        List<String> linkPool = new ArrayList();
+        Set<String> processedLinks = new HashSet<>();
+        linkPool.add("https://sina.cn");
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+            String link = linkPool.remove(linkPool.size() - 1);
 
-        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-            System.out.println(response1.getStatusLine());
-            HttpEntity entity1 = response1.getEntity();
-            // do something useful with the response body
-            // and ensure it is fully consumed
-            System.out.println(EntityUtils.toString(entity1));
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+
+            if ((link.contains("sina.cn") || link.contains("sina.com.cn")) && !link.contains("passport.sina.cn") && !link.contains("share.sina.cn") && (link.contains("news.sina.cn") || "https://sina.cn".equals(link))) {
+                // 这是我们感兴趣的，我们只处理新浪站内的连接
+                CloseableHttpClient httpclient = HttpClients.createDefault();
+
+                if (link.contains("//") && !link.contains("https:")) {
+                    link = "https:" + link;
+                }
+                System.out.println(link);
+
+                HttpGet httpGet = new HttpGet(link);
+                httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36");
+
+                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+
+                    System.out.println(response1.getStatusLine());
+                    HttpEntity entity1 = response1.getEntity();
+                    String html = EntityUtils.toString(entity1);
+
+                    Document doc = Jsoup.parse(html);
+                    ArrayList<Element> aTags = doc.select("a");
+                    for (Element aTag : aTags) {
+                        linkPool.add(aTag.attr("href"));
+                    }
+                    //假如这是一个新闻的详情页面，就存入数据库，否则，就什么也不做。
+                    ArrayList<Element> articleTags = doc.select("article");
+                    if (!articleTags.isEmpty()) {
+                        for (Element articleTag : articleTags) {
+                            String title = articleTags.get(0).child(0).text();
+                            System.out.println(title);
+                        }
+                    }
+                    processedLinks.add(link);
+                }
+
+
+            } else {
+                // 这是我们不感兴趣的，不处理它
+                continue;
+            }
+
+
         }
+
 
     }
 }
