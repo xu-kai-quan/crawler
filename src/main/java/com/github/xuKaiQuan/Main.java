871530fc8b94de580cfa4510f68c44a564059da9
XuKaiQuan <964262001@qq.com>
2021-01-29 13:45:22 +0800

    使用H2数据库实现数据存储与断点续传(重构)并通过spotbugs检查

--- src/main/java/com/github/xuKaiQuan/Main.java
+++ src/main/java/com/github/xuKaiQuan/Main.java
@@ -1,5 +1,6 @@
 package com.github.xuKaiQuan;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -13,17 +14,18 @@ import org.jsoup.nodes.Element;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 
 public class Main {
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
+
 
     private static List<String> loadUrlsFromDatabase(Connection connection, String sql) throws SQLException {
         List<String> results = new ArrayList<>();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
             while (resultSet.next()) {
                 results.add(resultSet.getString(1));
             }
@@ -31,62 +33,56 @@ public class Main {
         return results;
     }
 
+
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
     public static void main(String[] args) throws IOException, SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:h2:file:D:\\JAVAproject\\crawlerAndES\\crawler\\news","root","root");
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:D:\\JAVAproject\\crawlerAndES\\crawler\\news", USER_NAME, PASSWORD);
         while (true) {
             List<String> linkPool = loadUrlsFromDatabase(connection, "select link from LINKS_TO_BE_PROCESSED");
 
-//            Set<String> processedLinks = new HashSet<>(loadUrlsFromDatabase(connection, "select link from LINKS_ALREADY_PROCESSED"));
-
-            if (linkPool.isEmpty()){
+            if (linkPool.isEmpty()) {
                 break;
             }
 
             String link = linkPool.remove(linkPool.size() - 1);
+            insertLinkIntoDatabase(connection, link, "DELETE FROM LINKS_TO_BE_PROCESSED where link = ?");
 
-            try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM LINKS_TO_BE_PROCESSED where link = ?")) {
-                preparedStatement.setString(1,link);
-                preparedStatement.executeUpdate();
-
-            }
-
-            boolean flag = false;
-            try (PreparedStatement preparedStatement = connection.prepareStatement("select link from LINKS_ALREADY_PROCESSED where link = ?")) {
-                preparedStatement.setString(1,link);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while(resultSet.next()){
-                    flag = true;
-                }
-
-            }
-
-
-
-            if (flag) {
+            if (isLinkProcessed(connection, link)) {
                 continue;
             }
             if (isInterestingLink(link)) {
                 Document doc = httpGetAndParseHtml(link);
-                for (Element aTag : doc.select("a")) {
-                    String href = aTag.attr("href");
-                    try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO LINKS_TO_BE_PROCESSED (link) values(?)")) {
-                        preparedStatement.setString(1,href);
-                        preparedStatement.executeUpdate();
-                    }
-                }
-
+                parseUrlsFromPageAndStoreIntoDatabase(connection, doc);
                 //假如这是一个新闻的详情页面，就存入数据库，否则，就什么也不做。
                 storeIntoDatabaseIfItIsNewPage(doc);
 
-                try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO LINKS_ALREADY_PROCESSED (link) values(?)")) {
-                    preparedStatement.setString(1,link);
-                    preparedStatement.executeUpdate();
-                }
+                insertLinkIntoDatabase(connection, link, "INSERT INTO LINKS_ALREADY_PROCESSED (link) values(?)");
 
-//                processedLinks.add(link);
-            } else {
-                // 这是我们不感兴趣的，不处理它
             }
+        }
+    }
+
+    private static void parseUrlsFromPageAndStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            insertLinkIntoDatabase(connection, href, "INSERT INTO LINKS_TO_BE_PROCESSED (link) values(?)");
+        }
+    }
+
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("select link from LINKS_ALREADY_PROCESSED where link = ?")) {
+            preparedStatement.setString(1, link);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.next();
+            }
+        }
+    }
+
+    private static void insertLinkIntoDatabase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, link);
+            preparedStatement.executeUpdate();
         }
     }
 
